c4e4082b7f055aecb6a6572988d718cfaedfbcc0
peoovarasan123 <ksrpoovarasan634@gmail.com>
2021-08-16 19:04:17 +0530

    news design updated.

---
+++ app/src/main/java/com/master/navdrawerbottomnva/home/NewsExtendActivity.java
@@ -0,0 +1,14 @@
+package com.master.navdrawerbottomnva.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsExtendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_extend);
+    }
+}
